d63f2f529d6cd7f601cc1e9828e244a795d4f286
MirandaVillar <59941326+MirandaVillar@users.noreply.github.com>
2020-02-03 13:54:24 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+//Function that gets rid of the preloeader
+/*window.addEventListener('load', () => {
+  const preload = document.querySelector('.preload');
+  preload.classList.add('preload-finish');
+});*/
